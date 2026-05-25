7a6884c9b0e74bf1bfd24af3710925bba746772a
Mirae <mirae@example.com>
2026-05-25 11:48:15 -0300

    Add database compatibility check

--- server/index.ts
+++ server/index.ts
@@ -2,6 +2,7 @@ import path from "path";
 import 'dotenv/config';
 import express, { type Request, Response, NextFunction } from "express";
 import { registerRoutes } from "./routes";
+import { ensureDatabaseCompatibility } from "./db";
 import { serveStatic } from "./static";
 import { createServer } from "http";
 import { env } from 'process';
@@ -110,6 +111,7 @@ app.use((req, res, next) => {
 });
 
 (async () => {
+  await ensureDatabaseCompatibility();
   await registerRoutes(httpServer, app);
 
   app.use((err: any, _req: Request, res: Response, _next: NextFunction) => {
